b04b74c6b75aaeb273176c3e11fd7b1ee58c8c08
Liam Morrison <liammtech@gmail.com>
2026-03-06 10:20:25 +0000

    Add logic to tools.sql.get_single_record

--- tools/sql.py
+++ tools/sql.py
@@ -17,7 +17,33 @@ def get_single_record(
     return_columns: str | list[str] = "*",
     order_by: str = "StockCode"
 ) -> Row: 
-    pass
+    # Validation to go here
+
+    return_columns = ", ".join(return_columns)
+
+    sql = [f"SELECT {return_columns} FROM {table}"]
+    params = []
+
+    for col, val in criteria.items():
+        if val == None:
+            continue
+
+        if len(params == 0):
+            sql.append(f"WHERE {col} = ?")
+        else:
+            sql.append(f"AND {col} = ?")
+
+        params.append(val)
+
+    if table == "BomStructure" or table == "[BomStructure+]":
+        order_by = "ParentPart"
+
+    final_sql = " ".join(sql) + f" ORDER BY {order_by}"
+    print(final_sql)
+
+    with get_cursor() as cursor:
+        cursor.execute(final_sql, params)
+        return cursor.fetchone()
 
 def get_multiple_records(
     *, 
@@ -40,6 +66,5 @@ def set_multiple_records(
     table: str,
     post_data: dict[str, object]
 ):
-    
     pass
 
