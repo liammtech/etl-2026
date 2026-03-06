450ee85ce63bdc634c22375a552527a00585baf7
Liam Morrison <liammtech@gmail.com>
2026-03-06 09:59:42 +0000

    Add parameters to sql functions

--- tools/sql.py
+++ tools/sql.py
@@ -1,21 +1,45 @@
 from pyodbc import Row
 from db.connection import get_cursor
+from typing import Optional
 
 def _execute_select():
     pass
 
+# All below functions assume a single table
+# Getters with joins may be added at a later date when need becomes apparent
+
+# TODO: validate all sql function arguments
+
 def get_single_record(
-    *, # TODO: validate all arguments
+    *, 
     table: str,
     criteria: dict[str, object],
     return_columns: str | list[str] = "*",
-    order_by: str = "StockCode",
-) -> Row:
-    
+    order_by: str = "StockCode"
+) -> Row: 
     pass
 
 def get_multiple_records(
-        
+    *, 
+    table: str,
+    criteria: dict[str, object],
+    return_columns: str | list[str] = "*",
+    order_by: str = "StockCode"
 ) -> list[Row]:
+    pass
+
+def set_single_record(
+    *,
+    table: str,
+    post_data: dict[str, object]
+):
+    pass
+
+def set_multiple_records(
+    *,
+    table: str,
+    post_data: dict[str, object]
+):
+    
     pass
 
