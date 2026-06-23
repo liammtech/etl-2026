bdd8e069fa63d4ac8ec6dbd00020fc94fb6e640a
Liam Morrison <liammtech@gmail.com>
2026-06-23 11:53:41 +0100

    Revise tests and workflows subfolder naming and refactor

--- basilisc.py
+++ basilisc.py
@@ -1,14 +1,14 @@
 import sys, json
 
-from workflows.sku_workflows.reset_mrp_fields import reset_mrp, reset_mrp_range
-from workflows.sku_workflows.fix_volumetrics import fix_door_volumetrics, fix_door_volumetrics_range
-from workflows.wip_workflows.waterford_m2m import populate_waterford_m2ms
-from workflows.bom_workflows.fix_quantities import memp_std_single, lldr_std_single, jayl_std_single
-from workflows.bom_workflows.fix_quantities_multiple import memp_std_range, lldr_std_range, jayl_std_range
-from workflows.bom_workflows.stocked_to_mto_jpull import switch_jpull_stocked_to_mto, jpull_range_to_mto
-from workflows.bom_workflows.restore_linked_stock_codes import restore_linked_sku, restore_linked_skus_range
-from workflows.bom_workflows.create_records import create_invwarehouse_record, create_invmasterplus_record, create_invwarehouse_record_range, create_invmasterplus_record_range, create_full_sales_code_routings
-from workflows.bom_workflows.d_codes import sub_out_d_code, sub_in_d_code
+from workflows.sku.reset_mrp_fields import reset_mrp, reset_mrp_range
+from workflows.sku.fix_volumetrics import fix_door_volumetrics, fix_door_volumetrics_range
+from workflows.wip.waterford_m2m import populate_waterford_m2ms
+from workflows.bom.fix_quantities import memp_std_single, lldr_std_single, jayl_std_single
+from workflows.bom.fix_quantities_multiple import memp_std_range, lldr_std_range, jayl_std_range
+from workflows.bom.stocked_to_mto_jpull import switch_jpull_stocked_to_mto, jpull_range_to_mto
+from workflows.bom.restore_linked_stock_codes import restore_linked_sku, restore_linked_skus_range
+from workflows.bom.create_records import create_invwarehouse_record, create_invmasterplus_record, create_invwarehouse_record_range, create_invmasterplus_record_range, create_full_sales_code_routings
+from workflows.bom.d_codes import sub_out_d_code, sub_in_d_code
 from tools.sku_tools.palletisation import determine_pallet_spec
 
 from dotenv import load_dotenv
